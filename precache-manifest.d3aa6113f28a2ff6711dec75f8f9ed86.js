4bf7bb6c467f03fea688a1b41a614181301b4b57
Rai Medeiros <raismedeiros@gmail.com>
2019-06-26 23:21:55 -0300

    Updates

---
+++ precache-manifest.d3aa6113f28a2ff6711dec75f8f9ed86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606a9362b8284c42c24220384713b712",
+    "url": "/joker-norris/index.html"
+  },
+  {
+    "revision": "782b2e074cebce916c0e",
+    "url": "/joker-norris/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/joker-norris/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "782b2e074cebce916c0e",
+    "url": "/joker-norris/static/js/main.2ab9d922.chunk.js"
+  },
+  {
+    "revision": "ae378216b6a90f2f82c5",
+    "url": "/joker-norris/static/js/runtime~main.6ff6196b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/joker-norris/static/media/logo.5d5d9eef.svg"
+  }
+]);
