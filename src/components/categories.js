6a6b52e2bbc4f8a6ab5657c7aa2ed9432043ca73
Rai Medeiros <raismedeiros@gmail.com>
2019-06-27 08:40:40 -0300

    Estrutura inicial das categorias

---
+++ src/components/categories.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class Categories extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>animal</li>
+          <li>career</li>
+          <li>celebrity</li>
+          <li>dev</li>
+          <li>explicit</li>
+          <li>fashion</li>
+          <li>food</li>
+          <li>history</li>
+          <li>money</li>
+          <li>movie</li>
+          <li>music</li>
+          <li>political</li>
+          <li>religion</li>
+          <li>science</li>
+          <li>sport</li>
+          <li>travel</li>
+        </ul>
+      </div>
+    );
+  }
+}
---
+++ src/components/joke.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Joke extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Food</h2>
+        <img alt="ícone" src="https://assets.chucknorris.host/img/avatar/chuck-norris.png"></img>
+        <p>Chuck Norris drinks napalm to quell his heartburn.</p>
+        <p>2016-05-01 10:51:41.584544</p>
+      </div>
+    );
+  }
+}
